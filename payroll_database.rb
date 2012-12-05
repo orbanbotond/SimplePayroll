94cbabce7e4f2c4cc420bf334cfe9dab9d54d3a6
Jürgen Bickert <juergenbickert@gmail.com>
2012-12-05 15:15:33 +0100

    remove static methods from database

--- payroll_database.rb
+++ payroll_database.rb
@@ -36,41 +36,4 @@ class PayrollDatabase
   def get_all_employee_ids
     @employees.keys
   end
-
-
-  def self.employees
-    @employees ||= {}
-  end
-
-  def self.members
-    @members ||= {}
-  end
-
-  def self.get_employee(id)
-    employees[id]
-  end
-
-  def self.add_employee(id, employee)
-    employees[id] = employee
-  end
-
-  def self.delete_employee(id)
-    employees.delete(id)
-  end
-
-  def self.add_union_member(id, employee)
-    members[id] = employee
-  end
-
-  def self.get_union_member(id)
-    members[id]
-  end
-
-  def self.remove_union_member(id)
-    members.delete(id)
-  end
-
-  def self.get_all_employee_ids
-    employees.keys
-  end
 end
